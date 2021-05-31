5404a79047aacce74cc8467bd10723e08b3fd3d0
Tack2 <takumi236@gmail.com>
2021-06-01 01:17:50 +0900

    rename to mayarepacker

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ about = {}
 
 __version__ = re.search(
     r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',  # It excludes inline comment too
-    io.open('MayaAutoPackageReloader/__init__.py', encoding='utf_8_sig').read()).group(1)
+    io.open('src/mayarepacker/__init__.py', encoding='utf_8_sig').read()).group(1)
 
 
 setup(
-    name="MayaAutoPackageReloader",
+    name="mayarepacker",
     version=__version__,
 )
